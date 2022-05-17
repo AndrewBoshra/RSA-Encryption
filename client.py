7e2582378a10ddbccb7276f23e44f8957a465b0b
BishoyAtef <bishoyatef313@gmail.com>
2022-05-17 21:57:26 +0200

    no constrints on msg size

--- client.py
+++ client.py
@@ -12,20 +12,20 @@ import json
 init()
 
 # set the available colors
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
-    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
-    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
-    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
-]
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX,
+          Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX,
+          Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX,
+          Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+          ]
 
 # choose a random color for the client
 client_color = random.choice(colors)
 
 # server's IP address
-# if the server is not on this machine, 
+# if the server is not on this machine,
 # put the private (network) IP address (e.g 192.168.1.2)
 SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 5002 # server's port
+SERVER_PORT = 5002  # server's port
 
 # initialize TCP socket
 s = socket.socket()
@@ -44,15 +44,19 @@ print('Enter the public key of the receiver in the order n then e')
 publicMod = int(input('n: '))
 publicExp = int(input('e: '))
 
+
 def listen_for_messages():
     while True:
         data = s.recv(1024).decode()
         data = json.loads(data)
-        message = data.get("message")
+        ciphertextArray = data.get("message")
         senderName = data.get("sendername")
         if (senderName != name):
-            message = RSA.getPlaintext(message, privateMod, privateExp)
-            print('\n' + data.get("header") + message + data.get("footer"))
+            plaintextArray = ''
+            for ciphertext in ciphertextArray:
+                plaintextArray += RSA.getPlaintext(ciphertext, privateMod, privateExp)
+            print('\n' + data.get("header") + plaintextArray + data.get("footer"))
+
 
 # make a thread that listens for messages to this client & print them
 t = Thread(target=listen_for_messages)
@@ -63,16 +67,21 @@ t.start()
 
 while True:
     # input message we want to send to the server
-    message =  input()
+    message = input()
     # a way to exit the program
     if message.lower() == 'q':
         break
-    #encrypt message
-    ciphertext = RSA.getCiphertext(message, publicMod, publicExp)
+    # encrypt message
+    msgArray = RSA.splitString(message)
+    ciphertextArray = []
+    for msg in msgArray:
+        ciphertextArray.append(RSA.getCiphertext(msg, publicMod, publicExp))
+    # ciphertext = RSA.getCiphertext(message, publicMod, publicExp)
     # add the datetime, name & the color of the sender
-    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     # finally, send the message
-    data = json.dumps({"header": f"{client_color}[{date_now}] {name}: ", "message": ciphertext, "footer": f"{Fore.RESET}", "sendername": name})
+    data = json.dumps({"header": f"{client_color}[{date_now}] {name}: ",
+                      "message": ciphertextArray, "footer": f"{Fore.RESET}", "sendername": name})
     s.send(data.encode())
     print(f"{client_color}[{date_now}] {name}: {message}{Fore.RESET}")
 # close the socket
